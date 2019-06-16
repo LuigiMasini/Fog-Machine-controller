58c193e3d5c56b61c64b7fae3caf51ee26c70e93
Luigi Masini <luigi_masini@tiscali.it>
2019-06-16 23:00:31 +0200

    added pcb and circuit files

---
+++ scheda/board.cmd
@@ -0,0 +1,64 @@
+# Pin name action command file
+
+# Start of element NANO
+ChangePinName(NANO, 30, VIN)
+ChangePinName(NANO, 29, GND)
+ChangePinName(NANO, 28, RST)
+ChangePinName(NANO, 27, +5V)
+ChangePinName(NANO, 26, A0)
+ChangePinName(NANO, 25, A1)
+ChangePinName(NANO, 24, A2)
+ChangePinName(NANO, 23, A3)
+ChangePinName(NANO, 22, A4)
+ChangePinName(NANO, 21, A5)
+ChangePinName(NANO, 20, A6)
+ChangePinName(NANO, 19, A7)
+ChangePinName(NANO, 18, AREF)
+ChangePinName(NANO, 17, 3V3)
+ChangePinName(NANO, 16, D13)
+ChangePinName(NANO, 15, D12)
+ChangePinName(NANO, 14, D11)
+ChangePinName(NANO, 13, D10)
+ChangePinName(NANO, 12, D9)
+ChangePinName(NANO, 11, D8)
+ChangePinName(NANO, 10, D7)
+ChangePinName(NANO, 9, D6)
+ChangePinName(NANO, 8, D5)
+ChangePinName(NANO, 7, D4)
+ChangePinName(NANO, 6, D3)
+ChangePinName(NANO, 5, D2)
+ChangePinName(NANO, 4, GND)
+ChangePinName(NANO, 3, RST)
+ChangePinName(NANO, 2, D0/RX)
+ChangePinName(NANO, 1, D1/TX)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element PWR
+ChangePinName(PWR, 1, 1)
+ChangePinName(PWR, 2, 2)
+
+# Start of element BUTTON
+ChangePinName(BUTTON, 2, 2)
+ChangePinName(BUTTON, 1, 1)
+
+# Start of element RELAY
+ChangePinName(RELAY, 4, 4)
+ChangePinName(RELAY, 3, 3)
+ChangePinName(RELAY, 2, 2)
+ChangePinName(RELAY, 1, 1)
+
+# Start of element NTC
+ChangePinName(NTC, 1, 1)
+ChangePinName(NTC, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 3, 3)
+ChangePinName(R3, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
